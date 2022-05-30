6dfddbf4dc6c8bf574f67d7fe3cf2c3802fb38f0
JhonCastillo12 <jcastillo.trespimediosco@gmail.com>
2022-05-30 15:20:47 -0500

    feat: Initial structure of the application

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>ChallengeITGlobersRN</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,2 @@
+# Challenge-ITGlobers-RN
+ 
